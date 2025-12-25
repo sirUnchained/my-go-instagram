ba0d69bf4d0b84e28b3684e778411c0d6e39c496
sirunchained <bbehnam3831@gmail.com>
2025-12-25 12:32:03 +0330

    .

---
+++ cmd/migrate/000006_create_post_tag_table.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS posts_tags;
+
+DROP INDEX IF EXISTS idx_posts_tags_post;
+DROP INDEX IF EXISTS idx_posts_tags_tag;
---
+++ cmd/migrate/000006_create_post_tag_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS posts_tags (
+    id bigserial PRIMARY KEY,
+    post bigserial REFERENCES posts(id) ON DELETE CASCADE,
+    tag bigserial REFERENCES tags(id) ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT unique_post_tag UNIQUE (post, tag)
+);
+
+CREATE INDEX idx_posts_tags_post ON posts_tags(post);
+CREATE INDEX idx_posts_tags_tag ON posts_tags(file);
