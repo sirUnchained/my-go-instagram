b1a12327690b983fee74ca469313e17d0c5d1a2c
sirunchained <bbehnam3831@gmail.com>
2025-12-28 12:38:09 +0330

    well today i meet unset function in postgres! lets try it.

--- internal/storage/file_storage.go
+++ internal/storage/file_storage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/lib/pq"
 	"github.com/sirUnchained/my-go-instagram/internal/payloads"
 	"github.com/sirUnchained/my-go-instagram/internal/storage/models"
 )
@@ -12,11 +13,54 @@ type FileStore struct {
 	db *sql.DB
 }
 
-func (fs *FileStore) Create(ctx context.Context, userid int64, files []payloads.CreateFilePayload) {
-	query := `INSERT INTO files (filename, filepath, size_bytes, creator) VALUES ($1, $2, $3, $4) RETURNING id;`
+func (fs *FileStore) Create(ctx context.Context, userid int64, files []payloads.CreateFilePayload) ([]models.FileModel, error) {
+	n := len(files)
+	filenames := make([]string, n)
+	filepaths := make([]string, n)
+	sizes := make([]int, n)
+	creators := make([]int64, n)
+
+	for i, file := range files {
+		filenames[i] = file.Filename
+		filepaths[i] = file.Filepath
+		sizes[i] = file.SizeBytes
+		creators[i] = userid
+	}
+
+	query := `
+				INSERT INTO files (filename, filepath, size_bytes, creator) 
+					SELECT unnest($1::text[]), unnest($2::text[]), unnest($3::bigint[]), unnest($4::bigint[]) 
+					RETURNING id, filename, filepath, size_bytes, creator, created_at, updated_at;
+			`
+
+	rows, err := fs.db.QueryContext(ctx, query,
+		pq.Array(filenames),
+		pq.Array(filepaths),
+		pq.Array(sizes),
+		pq.Array(creators))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
 	var newFiles []models.FileModel
+	for rows.Next() {
+		var newFile models.FileModel
+		err := rows.Scan(
+			&newFile.Id,
+			&newFile.Filename,
+			&newFile.Filepath,
+			&newFile.SizeBytes,
+			&newFile.Creator,
+			&newFile.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
 
-	fs.db.QueryRowContext(ctx, query, files).Scan()
+		newFiles = append(newFiles, newFile)
+	}
+
+	return newFiles, nil
 
 }
