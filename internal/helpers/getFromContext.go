f5f01e150d0a1eb889d1eaa2d18515b14b89d56f
sirunchained <bbehnam3831@gmail.com>
2026-01-05 12:22:27 +0330

    .

--- internal/helpers/getFromContext.go
+++ internal/helpers/getFromContext.go
@@ -11,8 +11,3 @@ func GetUserFromContext(r *http.Request) *models.UserModel {
 	user, _ := r.Context().Value(global_varables.USER_CTX).(models.UserModel)
 	return &user
 }
-
-func GetUserByIdFromContext(r *http.Request) *models.UserModel {
-	user, _ := r.Context().Value(global_varables.TARGET_USER_CTX).(models.UserModel)
-	return &user
-}
